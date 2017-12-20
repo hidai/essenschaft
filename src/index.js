03cd4b8925a6fa0c715203898ab3528103833800
Kenichi Hidai <hidai@nmn.jp>
2017-12-20 10:51:52 +0000

    Use Roboto font for Material-UI

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import './reset.css'
+import './roboto-font.css'
 import * as firebase from "firebase";
 import config from './firebase-config';
 
