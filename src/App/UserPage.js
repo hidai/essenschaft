f3c6ea39dcccf770fa5e93f8ac7120853832c4bb
Kenichi Hidai <hidai@nmn.jp>
2017-12-20 09:35:18 +0000

    Use AppBar and Drawer

--- src/App/UserPage.js
+++ src/App/UserPage.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
 import SignOutButton from '../Auth/SignOutButton'
 import * as firebase from 'firebase';
+import AppBar from 'material-ui/AppBar';
+import Avatar from 'material-ui/Avatar';
+import Drawer from 'material-ui/Drawer';
 
 class UserPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
   render() {
     const user = firebase.auth().currentUser;
     return (
         <div>
-          <div>UserPage</div>
-          <ul>
-            <li>{user.providerId}</li>
-            <li>{user.uid}</li>
-            <li>{user.displayName}</li>
-            <li>{user.email}</li>
-            <img src={user.photoURL} alt="user"></img>
-          </ul>
-          <SignOutButton></SignOutButton>
+          <AppBar title="Lunch order"
+            onLeftIconButtonClick={this.handleToggle} />
+          <Drawer
+            docked={false}
+            width={200}
+            open={this.state.open}
+            onRequestChange={(open) => this.setState({open})}
+          >
+            <div>
+              <Avatar src={user.photoURL} />
+            </div>
+            <div>
+              {user.displayName}
+            </div>
+            <div>
+              {user.email}
+            </div>
+            <SignOutButton></SignOutButton>
+          </Drawer>
+
         </div>
     )
   }
