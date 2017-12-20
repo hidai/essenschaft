f0e4c5736281d93146b2312a4f0fbe28056ed557
Kenichi Hidai <hidai@nmn.jp>
2017-12-20 10:33:53 +0000

    Delete firebase-config.js

--- src/firebase-config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX.firebaseapp.com",
-  databaseURL: "https://XXXXXXXXXXXXXXXXXXXXXXXX.firebaseio.com",
-  projectId: "XXXXXXXXXXXXXXXXXXXXXXXX",
-  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX.appspot.com",
-  messagingSenderId: "XXXXXXXXXXXX"
-};
