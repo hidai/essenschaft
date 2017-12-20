f3c75d634451cc633688269ad7993812935d0bd7
Kenichi Hidai <hidai@nmn.jp>
2017-12-20 03:09:53 +0000

    Setup firebase

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXX.firebaseapp.com",
+  databaseURL: "https://XXXXXXXXXXXXXXXXXXXXXXXX.firebaseio.com",
+  projectId: "XXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXX.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXX"
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
 import './reset.css'
+import * as firebase from "firebase";
+import config from './firebase-config';
 
+firebase.initializeApp(config);
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
